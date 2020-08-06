218a6515cd2fc489fcc05c767cfb7010ae1a0a8d
RuliSlim <ruli0to8@gmail.com>
2020-08-06 15:34:14 +0700

    done 1. layout youtube video done 2. logic have to subs done

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,9 +1,9 @@
+import 'package:fastpedia/main.dart';
 import 'package:fastpedia/model/user.dart';
 import 'package:fastpedia/services/user_preferences.dart';
-import 'package:fastpedia/services/user_provider.dart';
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 
@@ -15,6 +15,20 @@ class DashBoard extends StatefulWidget {
 
 class _DashBoardState extends State<DashBoard> {
   Future userFuture;
+  Future<User> getUserData() => UserPreferences().getUser();
+
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
+  WebViewController _myController;
+
+  String isSubscribe;
+
+  Future<String> getDom () async {
+    String data = await _myController.evaluateJavascript("document.querySelectorAll(\"button.c3-material-button-button\")[5].innerText");
+    setState(() {
+      isSubscribe = data;
+    });
+    return data;
+  }
 
   @override
   void initState() {
@@ -22,20 +36,34 @@ class _DashBoardState extends State<DashBoard> {
     userFuture = getUserData();
   }
 
-  Future<User> getUserData() => UserPreferences().getUser();
 
   @override
   Widget build(BuildContext context) {
 
-    final webView = WebView(
-      initialUrl: 'https://m.youtube.com/watch?v=5u3pZZ6uvDo',
-      javascriptMode: JavascriptMode.unrestricted,
-      initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
-      
+    final webView = Stack(
+      children: <Widget>[
+        Positioned(
+          top: -Responsive.height(6, context),
+          height: Responsive.height(100, context),
+          width: Responsive.width(100, context),
+          child: WebView(
+            initialUrl: 'https://m.youtube.com/watch?v=_uQrJ0TkZlc',
+            javascriptMode: JavascriptMode.unrestricted,
+            initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
+            gestureNavigationEnabled: true,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+              _myController = webViewController;
+            },
+            onPageFinished: (url) {
+              _myController.evaluateJavascript('document.querySelectorAll(".scwnr-content")[2].style.display="none";');
+              _myController.evaluateJavascript('document.querySelectorAll(".scwnr-content")[3].style.display="none";');
+              _myController.evaluateJavascript('document.querySelectorAll(".mobile-topbar-header.cbox")[0].style.display="none";');
+            },
+          ),
+        ),
+      ],
     );
-
-
-//    User user = Provider.of<UserProvider>(context).user;
 
     return Scaffold(
         appBar: AppBar(
@@ -51,9 +79,33 @@ class _DashBoardState extends State<DashBoard> {
               return Text('ga ada data');
             }
           },
-        )
+        ),
+      floatingActionButton: _subsAction(),
     );
   }
 
-
+  _subsAction() {
+    return FloatingActionButton(
+      onPressed: () async {
+        var hasil = getDom();
+        hasil.then((value) {
+          print(isSubscribe.length);
+          if (isSubscribe.length == 13) {
+            Flushbar(
+              title: 'Failed!',
+              message: 'You Have To Subscribe!',
+              duration: Duration(seconds: 3),
+            ).show(context);
+          } else {
+            Flushbar(
+              title: 'Congrats!',
+              message: 'You Earn Point!',
+              duration: Duration(seconds: 3),
+            ).show(context);
+          }
+        });
+      },
+      child: Icon(Icons.save),
+    );
+  }
 }

c9af8dd2fe14f9f23ccf57e218748c80e8bc03db
RuliSlim <ruli0to8@gmail.com>
2020-08-13 19:09:34 +0700

    fixing bug when textfield is active

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -19,6 +19,30 @@ class _LoginPage extends State<LoginPage> {
   final formKey = new GlobalKey<FormState>();
   String _username, _password;
 
+  // states
+  bool isLogin = true;
+  bool isActive = false;
+
+  // check if text fields is active
+  FocusNode _focusUsername = new FocusNode();
+  FocusNode _focusPassword = new FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusUsername.addListener(_onFocusChange);
+    _focusPassword.addListener(_onFocusChange);
+  }
+
+  void _onFocusChange(){
+    debugPrint("Focus: "+_focusUsername.hasFocus.toString());
+    setState(() {
+      isActive = _focusPassword.hasFocus || _focusUsername.hasFocus ? true : false;
+    });
+  }
+
+  // end check text field
+
   @override
   void dispose() {
     usernameController.dispose();
@@ -30,29 +54,37 @@ class _LoginPage extends State<LoginPage> {
   Widget build(BuildContext context) {
     WebService webService = Provider.of<WebService>(context);
 
-    final usernameField = TextFormField(
-      obscureText: false,
-      autofocus: false,
-      autocorrect: false,
-      onSaved: (value) => _username = value,
-      onChanged: (value) => _username = value,
-      decoration: InputDecoration(
-        prefixIcon: Icon(Icons.person_outline),
-        labelText: 'username',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-      ),
+    final usernameField = Container(
+        width: Responsive.width(80, context),
+        child: TextFormField(
+          focusNode: _focusUsername,
+          obscureText: false,
+          autofocus: false,
+          autocorrect: false,
+          onSaved: (value) => _username = value,
+          onChanged: (value) => _username = value,
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.person_outline),
+            labelText: 'username',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          ),
+        )
     );
 
-    final passwordField = TextFormField(
-      obscureText: false,
-      autofocus: false,
-      autocorrect: false,
-      onSaved: (value) => _password = value,
-      onChanged: (value) => _password = value,
-      decoration: InputDecoration(
-        prefixIcon: Icon(Icons.lock),
-        labelText: 'password',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+    final passwordField = Container(
+      width: Responsive.width(80, context),
+      child: TextFormField(
+        focusNode: _focusPassword,
+        obscureText: false,
+        autofocus: false,
+        autocorrect: false,
+        onSaved: (value) => _password = value,
+        onChanged: (value) => _password = value,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.lock),
+          labelText: 'password',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        ),
       ),
     );
 
@@ -87,64 +119,165 @@ class _LoginPage extends State<LoginPage> {
       });
     };
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      resizeToAvoidBottomPadding: false,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            color: Color.fromRGBO(32, 150, 83, 1),
-            height: Responsive.height(15, context),
-            width: Responsive.width(100, context),
+    Column loginStandard = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          color: Color.fromRGBO(32, 150, 83, 1),
+          height: Responsive.height(15, context),
+          width: Responsive.width(100, context),
+        ),
+        Container(
+          height: Responsive.height(30, context),
+          width: Responsive.width(100, context),
+          padding: EdgeInsets.all(20),
+          child: Image(
+            image: AssetImage('Fast-logo.png'),
           ),
-          Container(
-            height: Responsive.height(30, context),
-            width: Responsive.width(100, context),
-            padding: EdgeInsets.all(20),
-            child: Image(
-              image: AssetImage('Fast-logo.png'),
-            ),
-          ),
-          Container(
-              width: Responsive.width(80, context),
-              height: Responsive.height(5, context),
-              child:
-              usernameField
+        ),
+        Container(
+            width: Responsive.width(80, context),
+            height: Responsive.height(5, context),
+            child:
+            usernameField
 //            TextField(
 //              obscureText: false,
 //              decoration: InputDecoration(labelText: 'username'),
 //              controller: usernameController,
 //            ),
-          ),
-          Container(
-              width: Responsive.width(80, context),
-              height: Responsive.height(5, context),
-              child:
-              passwordField
-//            TextField(
-//              obscureText: true,
-//              decoration: InputDecoration(labelText: 'password'),
-//              controller: passwordController,
-//            ),
-          ),
-          Container(
+        ),
+        Container(
             width: Responsive.width(80, context),
             height: Responsive.height(5, context),
-            child: RaisedButton(
-              onPressed: () {
-                doLogin();
-              },
-              child: Text('Login'),
+            child:
+            passwordField
+        ),
+        Container(
+          width: Responsive.width(80, context),
+          height: Responsive.height(5, context),
+          child: RaisedButton(
+            onPressed: () {
+              doLogin();
+            },
+            child: Text('Login'),
+          ),
+        ),
+        Container(
+            color: Color.fromRGBO(32, 150, 83, 1),
+            height: Responsive.height(25, context),
+            width: Responsive.width(100, context)
+        )
+      ],
+    );
+
+    // ModernDesign
+
+    Row textLoginOrRegister = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        FlatButton(
+          onPressed: () {
+            setState(() {
+              isLogin = true;
+            });
+          },
+          child: Text('Login',
+            style: TextStyle(
+                fontWeight: FontWeight.w900,
+                color: isLogin ? Colors.white : Colors.grey
             ),
           ),
-          Container(
-              color: Color.fromRGBO(32, 150, 83, 1),
-              height: Responsive.height(25, context),
-              width: Responsive.width(100, context)
+        ),
+        Text('|'),
+        FlatButton(
+          onPressed: () {
+            setState(() {
+              isLogin = false;
+            });
+          },
+          child: Text('Register',
+            style: TextStyle(
+                fontWeight: FontWeight.w900,
+                color: isLogin ? Colors.grey : Colors.white
+            ),
+          ),
+        )
+      ],
+    );
+
+    ButtonTheme buttonSignInOrSignUp = ButtonTheme(
+        minWidth: Responsive.width(80, context),
+        child: RaisedButton(
+          child: Text(isLogin ? 'Sign In' : ' Sign Up'),
+          padding: EdgeInsets.all(8.0),
+          textColor: Colors.white,
+          color: Colors.blue,
+          splashColor: Colors.green,
+          animationDuration: Duration(seconds: 1),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+              side: BorderSide(color: Colors.black)
+          ),
+          onPressed: () {
+            doLogin();
+          },
+        )
+    );
+
+    Column loginOrRegister = Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        textLoginOrRegister,
+        Padding(
+          padding: EdgeInsets.all(5.0),
+          child: usernameField,
+        ),
+        Padding(
+          padding: EdgeInsets.all(5),
+          child: passwordField,
+        ),
+        Padding(
+          padding: EdgeInsets.all(5),
+          child: buttonSignInOrSignUp,
+        )
+      ],
+    );
+
+    Column loginOrRegisterNotActive = Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        textLoginOrRegister,
+        usernameField,
+        passwordField,
+      ],
+    );
+
+    AnimatedContainer fields = AnimatedContainer(
+      duration: Duration(milliseconds: 1500),
+      curve: Curves.ease,
+      height: isActive ? 550 : 200,
+      child: isActive ? loginOrRegister : loginOrRegisterNotActive,
+      decoration: BoxDecoration(
+          color: Colors.amberAccent,
+          borderRadius: new BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30)
           )
-        ],
       ),
+    );
+
+    Column modernDesign = Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        fields,
+      ],
+    );
+
+
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        resizeToAvoidBottomPadding: false,
+        body: modernDesign
     );
   }
 }
